02540e0c2bfd1cf685b8b9ef752a97a6977a1e79
Ben Campbell <ben@scumways.com>
2015-02-05 15:54:27 +1300

    update win32 build script for ui+scripts dir

--- build_windows.sh
+++ build_windows.sh
@@ -7,12 +7,13 @@ OUT=steno_win32_$STAMP
 mkdir $OUT
 
 cp steno/steno.exe $OUT/
-RESOURCES="main.qml project.qml Query.qml help.html helper.js"
-for RES in $RESOURCES
-do
-    cp steno/$RES $OUT/
-done
+#RESOURCES="main.qml project.qml Query.qml help.html helper.js"
+#for RES in $RESOURCES
+#do
+#    cp steno/$RES $OUT/
+#done
 cp -r steno/scripts $OUT/
+cp -r steno/ui $OUT/
 
 
 windeployqt --verbose=1 --compiler-runtime --debug --qmldir=$OUT $OUT/steno.exe
@@ -21,6 +22,6 @@ windeployqt --verbose=1 --compiler-runtime --debug --qmldir=$OUT $OUT/steno.exe
 cp /c/Qt/5.3/mingw482_32/bin/libwinpthread-1.dll $OUT/
 
 
-echo "bundled. now do:"
-echo "scp steno.dmg ben@roland.mediastandardstrust.org:/srv/vhost/steno.mediastandardstrust.org/web/"
+echo "done. now copy to:"
+echo "ben@roland.mediastandardstrust.org:/srv/vhost/steno.mediastandardstrust.org/web/"
 
