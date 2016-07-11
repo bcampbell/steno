0fb6ef2cb37f3b7a848e74bcbedc530ab7994960
Ben Campbell <ben@scumways.com>
2016-07-11 14:52:57 +1200

    more osx build tweakage

--- bundle_osx.sh
+++ bundle_osx.sh
@@ -3,6 +3,9 @@ set -e
 
 OUT=.
 APP=$OUT/steno.app
+STAMP=$(date +"%Y-%m-%d")
+DMG=steno_mac_$STAMP.dmg
+
 rm -rf $APP
 rm -f steno.dmg
 
@@ -18,9 +21,33 @@ cp -r steno/scripts $APP/Contents/Resources/
 cp steno/slurp_sources.csv $APP/Contents/Resources/
 
 
-/usr/local/Cellar/qt5/5.3.2/bin/macdeployqt steno.app -verbose=1 -qmldir=steno.app/Contents/Resources -dmg
+macdeployqt steno.app -verbose=1 -qmldir=steno.app/Contents/Resources
 
+codesign --deep -s "Developer ID Application" $APP
+hdiutil create -srcfolder $APP $DMG
 
-echo "bundled. now do:"
-echo "scp steno.dmg ben@roland.mediastandardstrust.org:/srv/vhost/steno.mediastandardstrust.org/web/"
+exit 0
+
+# fix up borked framework bundles
+for fdir in $APP/Contents/Frameworks/Qt*
+do
+    f=$(basename $fdir .framework)
+    echo fix $f
+    pushd $APP/Contents/Frameworks/${f}.framework >/dev/null
+    pushd Versions >/dev/null
+    ln -s 5 Current
+    mv ../Resources 5/
+    cp /usr/local/Cellar/qt5/5.3.2/lib/${f}.framework/Contents/Info.plist 5/Resources/
+    popd >/dev/null
+    ln -s Versions/Current/${f} ${f}
+    ln -s Versions/Current/Resources Resources
+    popd >/dev/null
+done
+
+#codesign --deep -s "Developer ID Application" steno.app
+
+# hdiutil create -srcfolder $APP $DMG
+
+#echo "bundled. now do:"
+#echo "scp $DMG ben@roland.mediastandardstrust.org:/srv/vhost/steno.mediastandardstrust.org/web/"
 
