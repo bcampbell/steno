f01097794d2f5a86d3a1db31265e585a2e408646
Ben Campbell <ben@scumways.com>
2017-01-13 15:14:12 +1300

    update window build script

--- build_windows.sh
+++ build_windows.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 set -e
 
+# build script for building a 64bit version of steno under
+# msys2, using the msys2 Q1 binaries
+
 STAMP=$(date +"%Y-%m-%d")
-OUT=steno_win32_$STAMP
+OUT=steno_windows_64bit_$STAMP
+
+# where to find the dlls from msys2
+MSYSBIN=/c/msys64/mingw64/bin
+
 
 mkdir $OUT
 
+# TODO:
+# sanitycheck presence of bin/fasttext.exe
+
 cp steno/steno.exe $OUT/
-#RESOURCES="main.qml project.qml Query.qml help.html helper.js"
-#for RES in $RESOURCES
-#do
-#    cp steno/$RES $OUT/
-#done
 cp -r steno/scripts $OUT/
 cp -r steno/ui $OUT/
+cp -r steno/bin $OUT/
 cp steno/slurp_sources.csv $OUT/
 
-
+# use windeployqt to sort out all the Qt gubbins
 #windeployqt --verbose=1 --compiler-runtime --debug --qmldir=$OUT $OUT/steno.exe
 windeployqt --verbose=1 --compiler-runtime --release --qmldir=$OUT $OUT/steno.exe
 
-# some stuff missed by windeployqt:
-cp /c/Qt/5.3/mingw482_32/bin/libwinpthread-1.dll $OUT/
+# some stuff is missed by windeployqt:
+# we're using the Qt from msys2, so there are a whole bunch of other dlls
+# we need to pack up for non-msys2 systems...
+#  - see windows_extra_dlls.txt. List just via trial and error, running
+#    the exe and seeing what is missing
+#
+for a in `cat windows_extra_dlls.txt`;
+do
+    if [ ! -f $OUT/$a ]
+    then
+        echo "adding $a"
+        cp "$MSYSBIN/$a" "$OUT/$a"
+    fi
+done
 
-
-echo "done. now copy to:"
-echo "ben@roland.mediastandardstrust.org:/srv/vhost/steno.mediastandardstrust.org/web/"
 
