7c93aa42b02465f033d1777796ef0f1aac5f9f77
Ben Campbell <ben@scumways.com>
2018-01-07 22:25:51 +1300

    add startup window to open or create new project

--- gui/app.go
+++ gui/app.go
@@ -41,7 +41,56 @@ func (app *App) openProject(storePath string) error {
 
 func (app *App) Run(dbFilename string) error {
 	err := ui.Main(func() {
-		app.openProject(dbFilename)
+		if dbFilename == "" {
+			app.startupWindow()
+		} else {
+			app.openProject(dbFilename)
+		}
 	})
 	return err
+}
+
+func (app *App) startupWindow() {
+	window := ui.NewWindow("Steno startup", 400, 400, false)
+	window.SetMargined(true)
+
+	box := ui.NewVerticalBox()
+	box.SetPadded(true)
+
+	newButton := ui.NewButton("New (blank) Project")
+	openButton := ui.NewButton("Open existing project")
+
+	box.Append(newButton, false)
+	box.Append(openButton, false)
+
+	newButton.OnClicked(func(b *ui.Button) {
+		f := ui.SaveFile(window)
+		if f != "" {
+			err := app.openProject(f)
+			if err != nil {
+				ui.MsgBox(window, "Poop", err.Error())
+			} else {
+				window.Destroy()
+			}
+		}
+	})
+	openButton.OnClicked(func(b *ui.Button) {
+		f := ui.OpenFile(window)
+		if f != "" {
+			err := app.openProject(f)
+			if err != nil {
+				ui.MsgBox(window, "Poop", err.Error())
+			} else {
+				window.Destroy()
+			}
+		}
+	})
+
+	window.OnClosing(func(w *ui.Window) bool {
+		ui.Quit()
+		return true
+	})
+
+	window.SetChild(box)
+	window.Show()
 }
