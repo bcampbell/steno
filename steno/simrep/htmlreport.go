26bdd2c78382ddd886e51e37c1900aecae762f45
Ben Campbell <ben@scumways.com>
2018-08-27 09:51:02 +1200

    add simrep package and steno-similar cmd

---
+++ steno/simrep/htmlreport.go
@@ -0,0 +1,78 @@
+package simrep
+
+import (
+	"fmt"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"io"
+	"semprini/steno/steno/store"
+)
+
+func emitHeader(w io.Writer, opts *Opts) {
+
+	raw := `<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+body { /* max-width: 80rem; margin: auto; */ }
+.grp { border-bottom: 1px solid black; margin-top: 2rem; }
+.art1 { margin-left: 2rem; margin-bottom: 2rem; border-top: 1px solid black; }
+.diff { display: none; }
+.showdiffs:checked + .diff { display:block; }
+.content { background: #ffe; padding: 1rem; }
+</style>
+</head>
+<body>
+`
+	io.WriteString(w, raw)
+
+	fmt.Fprintf(w, "<pre>ngramSize=%d, minWords=%d, matchThreshold=%.02f</pre>\n",
+		opts.NGramSize, opts.MinWords, opts.MatchThreshold)
+}
+
+func emitFooter(w io.Writer) {
+	raw := `</body>
+</html>
+`
+	io.WriteString(w, raw)
+}
+
+func emitMatches(w io.Writer, art *store.Article, matching []*store.Article, metrics []float64) {
+
+	//
+	txt2 := tidy(art.PlainTextContent())
+	fmt.Fprintln(w, `<div class="grp">`)
+	fmt.Fprintf(w, `<pre>`)
+	fmt.Fprintf(w, "Headline: %s\n", art.Headline)
+	fmt.Fprintf(w, "URL: <a href=\"%s\">%s</a>\n", art.CanonicalURL, art.CanonicalURL)
+	fmt.Fprintf(w, "Pub: %s\n", art.Publication.Code)
+	fmt.Fprintf(w, "Content:\n")
+	fmt.Fprintln(w, `<div class="content">`)
+	fmt.Fprintf(w, "%s\n", txt2)
+	fmt.Fprintln(w, `</div>`)
+	fmt.Fprintf(w, `</pre>`)
+	for i, art1 := range matching {
+		f := metrics[i]
+
+		// get the matching article
+		txt1 := tidy(art1.PlainTextContent())
+		fmt.Fprintln(w, `<div class="art1">`)
+		fmt.Fprintf(w, "<pre>\n")
+		fmt.Fprintf(w, "Match Factor: %f\n", f)
+		fmt.Fprintf(w, "Headline: %s\n", art1.Headline)
+		fmt.Fprintf(w, "URL: <a href=\"%s\">%s</a>\n", art1.CanonicalURL, art1.CanonicalURL)
+		fmt.Fprintf(w, "Pub: %s\n", art1.Publication.Code)
+		fmt.Fprintf(w, "</pre>\n")
+		fmt.Fprintln(w, `<label>show diff</label><input class="showdiffs" type="checkbox" />`)
+		fmt.Fprintln(w, `<div class="diff content"><pre>`)
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(txt1, txt2, false)
+		diffs = dmp.DiffCleanupSemantic(diffs)
+		fmt.Fprintln(w, dmp.DiffPrettyHtml(diffs))
+		fmt.Fprintln(w, `</pre></div>`)
+		fmt.Fprintf(w, "</div>\n")
+	}
+	fmt.Fprintf(w, "</div> <!-- end .grp -->\n")
+}
---
+++ steno/simrep/simrep.go
@@ -0,0 +1,139 @@
+package simrep
+
+// generate a similarity report between two steno databases...
+
+import (
+	"os"
+	"semprini/sim"
+	"semprini/steno/steno/store"
+	"strings"
+)
+
+type Opts struct {
+	Verbose        bool
+	MinWords       int
+	NGramSize      int
+	MatchThreshold float64
+	IgnoreSameID   bool
+	//
+	Dbug store.Logger
+}
+
+func Run(db1 *store.Store, db2 *store.Store, opts *Opts) error {
+
+	dbug := opts.Dbug
+
+	// index the first store
+	dbug.Printf("building similarity index...\n")
+	idx, err := buildIndex(db1, opts)
+	if err != nil {
+		return err
+	}
+
+	dbug.Printf("matching against index...\n")
+
+	matches, err := findMatches(idx, db2, opts)
+	if err != nil {
+		return err
+	}
+
+	// output report
+
+	dbug.Printf("output report...\n")
+	emitHeader(os.Stdout, opts)
+	// for each article...
+	for art2ID, m := range matches {
+		// fetch the article
+		foo, err := db2.Fetch(art2ID)
+		if err != nil {
+			return err
+		}
+		art := foo[0]
+
+		// fetch all the articles it matched
+		matchArtIDs := make([]store.ArtID, len(m))
+		metrics := make([]float64, len(m))
+		for i, dm := range m {
+			matchArtIDs[i] = store.ArtID(dm.ID)
+			metrics[i] = dm.Factor
+		}
+		matchingArts, err := db1.Fetch(matchArtIDs...)
+		if err != nil {
+			return err
+		}
+
+		emitMatches(os.Stdout, art, matchingArts, metrics)
+
+	}
+
+	emitFooter(os.Stdout)
+	return nil
+}
+
+func tidy(s string) string {
+	lines := strings.Split(s, "\n")
+	out := make([]string, 0, len(lines))
+	for _, l := range lines {
+		l = strings.TrimSpace(l)
+		if len(l) > 0 {
+			out = append(out, l)
+		}
+	}
+
+	return strings.Join(out, "\n")
+}
+
+func buildIndex(db *store.Store, opts *Opts) (*sim.Index, error) {
+	idx, err := sim.NewIndex(opts.NGramSize)
+	if err != nil {
+		return nil, err
+	}
+	it := db.IterateAllArts()
+	for it.Next() {
+		art := it.Cur()
+		txt := art.PlainTextContent()
+
+		nWords := len(strings.Fields(txt))
+		if nWords < opts.MinWords {
+			continue
+		}
+		idx.AddDoc(sim.DocID(art.ID), txt)
+	}
+	if it.Err() != nil {
+		return nil, it.Err()
+	}
+	idx.Finalise()
+	return idx, nil
+}
+
+func findMatches(idx *sim.Index, db *store.Store, opts *Opts) (map[store.ArtID][]sim.DocMatch, error) {
+	matches := map[store.ArtID][]sim.DocMatch{}
+
+	it := db.IterateAllArts()
+	for it.Next() {
+		art := it.Cur()
+		txt := art.PlainTextContent()
+		nWords := len(strings.Fields(txt))
+		if nWords < opts.MinWords {
+			continue
+		}
+		hits := idx.Match(txt, opts.MatchThreshold)
+		if opts.IgnoreSameID {
+			tmp := make([]sim.DocMatch, 0, len(hits))
+			for _, dm := range hits {
+				if store.ArtID(dm.ID) != art.ID {
+					tmp = append(tmp, dm)
+				}
+			}
+			hits = tmp
+		}
+
+		if len(hits) > 0 {
+			matches[art.ID] = hits
+		}
+	}
+	if it.Err() != nil {
+		return nil, it.Err()
+	}
+	return matches, nil
+}
